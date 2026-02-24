45cd5fdcf98dac8fc929e7041ece11f9a4046392
advgnd <advaith@gundu.me>
2026-02-24 08:42:46 -0500

    fix: more actions fixes

---
+++ packaging/deb/postinst.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+mkdir -p /var/lib/gaze/users
+mkdir -p /opt/gaze/models
+systemctl daemon-reload
+dbus-send --system --type=method_call --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.ReloadConfig 2>/dev/null || true
+systemctl enable --now gazed
